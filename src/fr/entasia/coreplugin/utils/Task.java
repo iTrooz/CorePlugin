5c9f5da14000ed0b16246b34a2074b6451484398
iTrooz_ <team.mortelgames@gmail.com>
2020-06-06 20:14:38 +0200

    TPS warning

--- src/fr/entasia/coreplugin/utils/Task.java
+++ src/fr/entasia/coreplugin/utils/Task.java
@@ -1,6 +1,9 @@
 package fr.entasia.coreplugin.utils;
 
 import fr.entasia.apis.ServerUtils;
+import net.minecraft.server.v1_12_R1.MinecraftServer;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
@@ -11,13 +14,17 @@ public class Task extends BukkitRunnable {
 	public void run() {
 		File a = new File("logs/latest.log");
 		if(a.exists()){
-			double len = a.length()/1024f/1024f;
-			if(len>1) ServerUtils.permMsg("logs.logs",
-					"§cLes logs font plus de §8"+Math.round(len*100)/100f+"§cMo !");
+			double d = a.length()/1024f/1024f;
+			if(d>1) ServerUtils.permMsg("logs.logs",
+					"§cLes logs font plus de §8"+Math.round(d*100)/100f+"§cMo !");
 
-			len = (Runtime.getRuntime().freeMemory()/(float)Runtime.getRuntime().maxMemory())/1073741824f;
-			if(len>0.95)ServerUtils.permMsg("logs.ram",
-					"§cLa mémoire est à §8"+Math.round(len*100)/100f+"§c% !");
+			d = (Runtime.getRuntime().freeMemory()/(float)Runtime.getRuntime().maxMemory())/1073741824f;
+			if(d>0.90)ServerUtils.permMsg("logs.ram",
+					"§cLa mémoire est à §8"+Math.round(d*100)/100f+"§c% !");
+
+			d = Bukkit.getServer().getTPS()[0];
+			if(d<19.5)ServerUtils.permMsg("logs.tps",
+					"§cLes TPS sont à moins de §8"+Math.round(d*100)/100f+"§c% !");
 
 		}else ServerUtils.permMsg("logs.logs",
 				"§cImpossible de trouver le fichiers des logs ! Redémarre le serveur pour réparer ce problème");
