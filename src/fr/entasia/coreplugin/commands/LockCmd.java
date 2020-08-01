185fd5df6d2b580fad2dd076d29370018e1348ed
iTrooz_ <team.mortelgames@gmail.com>
2020-08-01 14:36:41 +0200

    perm change

--- src/fr/entasia/coreplugin/commands/LockCmd.java
+++ src/fr/entasia/coreplugin/commands/LockCmd.java
@@ -17,7 +17,7 @@ public class LockCmd implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(!(sender instanceof Player))return false;
 			Player p = (Player) sender;
-			if(p.hasPermission("staff.lockdown")){
+			if(p.hasPermission("staff.lockdown.use")){
 
 				if (args.length != 0 && (args[0].equals("status") || args[0].equals("statut"))) {
 					if (Main.lockdown == null)
